dc201d9a7444f49b14da5c4b91676dfdcbbf272d
EricJakovac <jakokovaceric@gmail.com>
2023-06-21 16:32:19 +0200

    Commit

--- app/src/main/java/com/example/timetodo/Utils/DatabaseHandler.java
+++ app/src/main/java/com/example/timetodo/Utils/DatabaseHandler.java
@@ -13,48 +13,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
-    private static final int VERSION = 3;
+    private static final int VERSION = 4;
     private static final String NAME = "ToDoListDatabase";
     private static final String TODO_TABLE = "todo";
     private static final String ID = "id";
     private static final String TASK = "task";
     private static final String STATUS = "status";
-    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TASK + " TEXT, " + STATUS + " INTEGER) ";
+    private static final String DUE_TIME = "due_time";
+    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TASK + " TEXT, " + STATUS + " INTEGER, " + DUE_TIME + " TEXT) ";
     private SQLiteDatabase db;
 
-    public DatabaseHandler(Context context){
+    public DatabaseHandler(Context context) {
         super(context, NAME, null, VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db){
+    public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TODO_TABLE);
     }
+
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        //Drop the older tables
-        db.execSQL("DROP TABLE " + TODO_TABLE);
-        //Create tables again
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TODO_TABLE);
         onCreate(db);
     }
 
-    public void openDatabase(){
+    public void openDatabase() {
         db = this.getWritableDatabase();
     }
 
-    public void insertTask(ToDoModel task){
+    public void insertTask(ToDoModel task) {
         ContentValues cv = new ContentValues();
         cv.put(TASK, task.getTask());
         cv.put(STATUS, 0);
+        cv.put(DUE_TIME, task.getDueTime());
         db.insert(TODO_TABLE, null, cv);
     }
 
     @SuppressLint("Range")
-    public List<ToDoModel> getAllTasks(){
+    public List<ToDoModel> getAllTasks() {
         List<ToDoModel> taskList = new ArrayList<>();
         Cursor cur = null;
         db.beginTransaction();
-        try{
+        try {
             cur = db.query(TODO_TABLE, null, null, null, null, null, null);
             if (cur != null) {
                 if (cur.moveToFirst()) {
@@ -63,39 +64,37 @@ public class DatabaseHandler extends SQLiteOpenHelper {
                         task.setId(cur.getInt(cur.getColumnIndex(ID)));
                         task.setTask(cur.getString(cur.getColumnIndex(TASK)));
                         task.setStatus(cur.getInt(cur.getColumnIndex(STATUS)));
+                        task.setDueTime(cur.getString(cur.getColumnIndex(DUE_TIME)));
                         taskList.add(task);
-                    }while(cur.moveToNext());
+                    } while (cur.moveToNext());
                 }
             }
-        }
-        finally{
+        } finally {
             db.endTransaction();
-            cur.close();
+            if (cur != null) {
+                cur.close();
+            }
         }
         return taskList;
     }
 
-    public void updateStatus(int id, int status){
+    public void updateStatus(int id, int status) {
         ContentValues cv = new ContentValues();
         cv.put(STATUS, status);
-        db.update(TODO_TABLE, cv, ID + "=?", new String[] {String.valueOf(id)});
+        db.update(TODO_TABLE, cv, ID + "=?", new String[]{String.valueOf(id)});
     }
 
     public void updateTask(ToDoModel task) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
         ContentValues values = new ContentValues();
         values.put(TASK, task.getTask());
         values.put(STATUS, task.getStatus());
+        values.put(DUE_TIME, task.getDueTime());
 
-        // Update the task in the database based on the task ID
         db.update(TODO_TABLE, values, ID + " = ?", new String[]{String.valueOf(task.getId())});
-
-        db.close();
     }
 
-    public void deleteTask(int id){
-        db.delete(TODO_TABLE, ID + "=?", new String[] {String.valueOf(id)});
+    public void deleteTask(int id) {
+        db.delete(TODO_TABLE, ID + "=?", new String[]{String.valueOf(id)});
     }
 
     public void updateTask(int id, String text) {
